22a826947ca0aed455dc1d2534c10d53229bb5dc
Maher-Elmair <maher.elmair.dev@gmail.com>
2025-08-31 18:08:17 +0300

    Add initial completed Dashboard (Home) page – project still in progress

---
+++ src/Providers/data/fatch-wrapper.ts
@@ -0,0 +1,59 @@
+import { GraphQLFormattedError } from "graphql";
+
+type Error = {
+  message: string;
+  statusCode: string;
+};
+
+const customFetch = async (url: string, options: RequestInit) => {
+  const accessToken = localStorage.getItem("access_token");
+
+  const headers = options.headers as Record<string, string>;
+
+  return await fetch(url, {
+    ...options,
+    headers: {
+      ...headers,
+      Authorization: headers?.Authorization || `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      "Apollo-Require-Preflight": "true",
+    },
+  });
+};
+
+const getGraphQLErrors = (
+  body: Record<"errors", GraphQLFormattedError[] | undefined>
+): Error | null => {
+  if (!body) {
+    return {
+      message: "UNknow error",
+      statusCode: "INTERNAL_SERVER_ERROR",
+    };
+  }
+  if ("errors" in body) {
+    const erros = body?.errors;
+
+    const messages = erros?.map((error) => error.message)?.join("");
+    const code = erros?.[0]?.extensions?.code;
+
+    return {
+      message: messages || JSON.stringify(erros),
+      statusCode: code || 500,
+    };
+  }
+  return null;
+};
+
+export const fetchWrapper = async (url: string, options: RequestInit) => {
+  const response = await customFetch(url, options);
+
+  const responseClone = response.clone();
+const body = await responseClone.json();
+
+  const error = getGraphQLErrors(body);
+
+  if (error) {
+    throw error;
+  }
+  return response;
+}; 
